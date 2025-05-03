3638b9c1c2e3cee8e6387f60ff3bb0db8b0b8be3
kkizuh <kristian.cogotkov@gmail.com>
2025-05-03 13:38:55 +0300

    Update database port configuration to 8139 in database.py and docker-compose.yml

--- app/database.py
+++ app/database.py
@@ -8,7 +8,7 @@ load_dotenv()
 DB_USER = os.getenv("POSTGRES_USER")
 DB_PASS = os.getenv("POSTGRES_PASSWORD")
 DB_HOST = os.getenv("DB_HOST", "localhost")
-DB_PORT = os.getenv("DB_PORT", "5432")
+DB_PORT = os.getenv("DB_PORT", "8139")
 DB_NAME = os.getenv("DB_NAME")
 
 DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
